cf9758e026ca69b6b272ebe5d59b1efd0173f9d1
Muhammad Fithra Rizki <167663689+fithrarzk@users.noreply.github.com>
2025-10-18 17:08:16 +0700

    fix:testing

--- test/sample_program.pas
+++ test/sample_program.pas
@@ -2,7 +2,7 @@ program Hello;
 var
   a, b: integer;
 begin
-  a := 23e-12;
+  a := 2;
   b := a + 10;
   writeln('Result = ', b);
 end.
--- test/test2.pas
+++ test/test2.pas
@@ -3,11 +3,9 @@ var
   i: integer;
   arr: array[5..10] of integer;
 begin
-  { Inisialisasi nilai array }
   for i := 5 to 10 do
     arr[i] := i * 2;
 
-  { Tampilkan nilai array }
   for i := 5 to 10 do
     writeln('arr[', i, '] = ', arr[i]);
 end.
